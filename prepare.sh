415c4a79a89ae686708f39f785bb2d00f767e2e3
Jihoon Oh <oh@jsk.imi.i.u-tokyo.ac.jp>
2024-01-17 21:19:42 +0900

    fix prepare and requirements for tapnet and jax

--- prepare.sh
+++ prepare.sh
@@ -4,3 +4,4 @@ git submodule update --init --recursive
 sudo apt install -y python3.9 python3.9-dev python3.9-venv
 python3.9 -m pip install -U numpy
 python3.9 -m pip install torch==2.1.1 torchvision==0.16.1 torchaudio==2.1.1 --index-url https://download.pytorch.org/whl/cu121
+python3.9 -m pip install "jax[cuda12_pip]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
