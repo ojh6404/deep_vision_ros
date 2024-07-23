e9728e1a739298b999153fa88da09208c143d240
Jihoon Oh <oh@jsk.imi.i.u-tokyo.ac.jp>
2024-07-23 23:05:15 +0900

    fix natten

--- tracking_ros/prepare.sh
+++ tracking_ros/prepare.sh
@@ -5,3 +5,4 @@ git submodule update --init --recursive
 sudo apt install -y python3.9 python3.9-dev python3.9-venv
 python3.9 -m pip install numpy==1.26.4 psutil==5.9.8
 python3.9 -m pip install torch torchvision
+CUDA_ARCH="8.0;8.6" python3.9 -m pip install natten==0.17.1+torch230cu121 -f https://shi-labs.com/natten/wheels/
