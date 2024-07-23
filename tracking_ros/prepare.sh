b5e8183fc644c2d48f94d5bac63bab58848e9ce7
Jihoon Oh <oh@jsk.imi.i.u-tokyo.ac.jp>
2024-07-23 21:41:06 +0900

    fix prepare script and requirements

--- tracking_ros/prepare.sh
+++ tracking_ros/prepare.sh
@@ -4,4 +4,4 @@ pip3 install gdown # Install gdown to download the model
 git submodule update --init --recursive
 sudo apt install -y python3.9 python3.9-dev python3.9-venv
 python3.9 -m pip install numpy<2 psutil==5.9.8
-python3.9 -m pip install torch==2.2.2 torchvision==0.17.2 torchaudio==2.2.2 --index-url https://download.pytorch.org/whl/cu121
+python3.9 -m pip install torch torchvision
