846c55f24361361d8b39defdf578fc600e9da5a5
Jihoon Oh <oh@jsk.imi.i.u-tokyo.ac.jp>
2024-07-23 22:04:22 +0900

    fix prepare and drop cuda 11.3

--- tracking_ros/prepare.sh
+++ tracking_ros/prepare.sh
@@ -3,5 +3,5 @@
 pip3 install gdown # Install gdown to download the model
 git submodule update --init --recursive
 sudo apt install -y python3.9 python3.9-dev python3.9-venv
-python3.9 -m pip install numpy<2 psutil==5.9.8
+python3.9 -m pip install "numpy<2" "psutil==5.9.8"
 python3.9 -m pip install torch torchvision
